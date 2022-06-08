fc37ab0f1f46d6155dc33d50de5a952f5c96b77e
Thomas Aung <92353124+Thomasaung12345@users.noreply.github.com>
2022-06-08 09:21:13 +0630

    Update clear.py

--- clear.py
+++ clear.py
@@ -1,3 +1,3 @@
 with open("ids.html", 'w') as f:
     f.truncate(0)
-    f.write('<title>Saved Zoom Links</title> <link href="style.css" rel="stylesheet"> <body style="background-color:black;"> <h2> Zoom Links!</h2>')
+    f.write('<title>Saved Zoom Links</title> <meta http-equiv="refresh" content="5"> <link href="style.css" rel="stylesheet"> <body style="background-color:black;"> <h2> Zoom Links!</h2>')
